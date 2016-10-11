cfc146f58f818318661d314e500c30bac0b6dbf2
Sallario <alister@hartwells.plus.com>
2016-10-11 13:01:43 +0100

    Tutorial 3
    Halfway through setup

--- Alister Hartwell AGP Unit/main.cpp
+++ Alister Hartwell AGP Unit/main.cpp
@@ -2,29 +2,52 @@
 #include <d3d11.h>
 #include <d3dx11.h>
 #include <dxerr.h>
+
+
+#define _XM_NO_INTRINSICS_
+#define XM_NO_ALIGNMENT
+#include <xnamath.h>
 //////////////////////////////////////////////////////////////////////////////////////
 // Global Variables
 //////////////////////////////////////////////////////////////////////////////////////
 HINSTANCE g_hInst = NULL;
-HWND g_hWnd = NULL;
+HWND g_hWnd =		NULL;
 // Rename for each tutorial
 char g_TutorialName[100] = "Alister Hartwell Tutorial 2\0";
 
-D3D_DRIVER_TYPE g_driverType = D3D_DRIVER_TYPE_NULL;
-D3D_FEATURE_LEVEL g_featureLevel = D3D_FEATURE_LEVEL_11_0;
-ID3D11Device* g_pD3DDevice = NULL;
-ID3D11DeviceContext* g_pImmediateContext = NULL;
-IDXGISwapChain* g_pSwapChain = NULL;
+D3D_DRIVER_TYPE			g_driverType = D3D_DRIVER_TYPE_NULL;
+D3D_FEATURE_LEVEL		g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+ID3D11Device*			g_pD3DDevice = NULL;
+ID3D11DeviceContext*	g_pImmediateContext = NULL;
+IDXGISwapChain*			g_pSwapChain = NULL;
 ID3D11RenderTargetView* g_pBackBufferRTView = NULL;
+
+
+// Maths stuff
+
+ID3D11Buffer *			g_pVertexBuffer;
+ID3D11VertexShader*		g_pVertexShader;
+ID3D11PixelShader*		g_pPixelShader;
+ID3D11InputLayout*		g_pInputLayout;
+
+// Vertex Structure
+struct POS_COL_VERTEX
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Col;
+};
 //////////////////////////////////////////////////////////////////////////////////////
 // Forward declarations
 //////////////////////////////////////////////////////////////////////////////////////
-HRESULT InitialiseWindow(HINSTANCE hInstance, int nCmdShow);
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HRESULT		InitialiseWindow(HINSTANCE hInstance, int nCmdShow);
+LRESULT		CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-HRESULT InitialiseD3D();
-void ShutDownD3D();
-void RenderFrame(void);
+HRESULT		InitialiseD3D();
+HRESULT		InitialiseGraphics(void);
+
+void		ShutDownD3D();
+void		RenderFrame(void);
+
 //////////////////////////////////////////////////////////////////////////////////////
 // Entry point to the program. Initializes everything and goes into a message processing
 // loop. Idle time is used to render the scene.
@@ -33,6 +56,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 {
 	UNREFERENCED_PARAMETER(hPrevInstance); // hPrevInstance is not neded therefore kept as null
 	UNREFERENCED_PARAMETER(lpCmdLine);
+
+
 	if (FAILED(InitialiseWindow(hInstance, nCmdShow)))
 	{
 		//DXTRACE_MSG("Failed to create Window");
@@ -42,6 +67,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	if (FAILED(InitialiseD3D()))
 	{
 		//DXTRACE_MSG("Failed to create Device");
+		return 0;
+	}
+
+	if (FAILED(InitialiseGraphics()))
+	{
+		//DXTRACE_MSG("Failed to create Graphics");
 		return 0;
 	}
 
@@ -73,24 +104,34 @@ HRESULT InitialiseWindow(HINSTANCE hInstance, int nCmdShow)
 	char Name[100] = "Alister Hartwell AGP Unit\0";
 	// Register class
 	WNDCLASSEX wcex = { 0 };
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = WndProc;
-	wcex.hInstance = hInstance;
-	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.cbSize =			sizeof(WNDCLASSEX);
+	wcex.style =			CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc =		WndProc;
+	wcex.hInstance =		hInstance;
+	wcex.hCursor =			LoadCursor(NULL, IDC_ARROW);
 	// wcex.hbrBackground = (HBRUSH )( COLOR_WINDOW + 1); // Needed for non-D3D apps
-	wcex.lpszClassName = Name;
+	wcex.lpszClassName =	Name;
+
+
 	if (!RegisterClassEx(&wcex)) return E_FAIL;
 	// Create window
 	g_hInst = hInstance;
 	RECT rc = { 0, 0, 1280, 720 }; // Set window size
+
+
 	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
 	g_hWnd = CreateWindow(Name, g_TutorialName, WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left,
 		rc.bottom - rc.top, NULL, NULL, hInstance, NULL);
+
+
 	if (!g_hWnd)
 		return E_FAIL;
+
+
 	ShowWindow(g_hWnd, nCmdShow);
+
 	return S_OK;
 }
 //////////////////////////////////////////////////////////////////////////////////////
@@ -99,23 +140,29 @@ HRESULT InitialiseWindow(HINSTANCE hInstance, int nCmdShow)
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
-	HDC hdc;
+	HDC			hdc;
 	switch (message)
 	{
+
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 		EndPaint(hWnd, &ps);
 		break;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
+
 	case WM_KEYDOWN:
 		if (wParam == VK_ESCAPE)
 			DestroyWindow(g_hWnd);
+
 		return 0;
+
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
+
 	return 0;
 }
 
@@ -124,12 +171,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 //////////////////////////////////////////////////////////////////////////////////////
 HRESULT InitialiseD3D()
 {
-	HRESULT hr = S_OK;
-	RECT rc;
+	HRESULT hr =	S_OK;
+	RECT			rc;
 	GetClientRect(g_hWnd, &rc);
-	UINT width = rc.right - rc.left;
-	UINT height = rc.bottom - rc.top;
-	UINT createDeviceFlags = 0;
+
+	UINT			width = rc.right - rc.left;
+	UINT			height = rc.bottom - rc.top;
+	UINT			createDeviceFlags = 0;
 
 #ifdef _DEBUG
 	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
@@ -206,7 +254,9 @@ HRESULT InitialiseD3D()
 	viewport.Height = height;
 	viewport.MinDepth = 0.0f;
 	viewport.MaxDepth = 1.0f;
-	g_pImmediateContext->RSSetViewports(1, &viewport);
+
+	g_pImmediateContext->RSSetViewports(1, &viewport);
+
 
 	return S_OK;
 }
@@ -221,13 +271,67 @@ void ShutDownD3D()
 	if (g_pD3DDevice) g_pD3DDevice->Release();
 }
 
-// Render frame
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Init Graphics
+//////////////////////////////////////////////////////////////////////////////////////
+HRESULT InitialiseGraphics() //03-01
+{
+	HRESULT hr = S_OK;
+
+	// Define vertices of a triangle - screen coords -1.0 - +1.0
+	POS_COL_VERTEX vertices[] =
+	{
+
+	{ XMFLOAT3(0.9f,0.9f,0.0f),			XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f) },
+	{ XMFLOAT3(0.9f,-0.9f,0.0f),		XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
+	{ XMFLOAT3(0.9f,-0.9f,0.0f),		XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f) }
+
+	};
+
+	// Set up and create vertex buffer
+	D3D11_BUFFER_DESC			 bufferDesc;
+	ZeroMemory(&bufferDesc, sizeof(bufferDesc));
+
+	bufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	bufferDesc.ByteWidth = sizeof(POS_COL_VERTEX) * 3;
+	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+	hr = g_pD3DDevice->CreateBuffer(&bufferDesc, NULL, &g_pVertexBuffer);
+
+	if (FAILED(hr))
+
+	{
+		return hr;
+	}
+	///////// FINISH THIS FROM PAGE 4 OF TUTORIAL YOU LAZY FUCK
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Render Frame
+//////////////////////////////////////////////////////////////////////////////////////
 void RenderFrame(void)
 {
 	// Clear the back buffer - choose a colour you like
 	float rgba_clear_colour[4] = { 0.1f, 0.2f, 0.6f, 1.0f };
 	g_pImmediateContext->ClearRenderTargetView(g_pBackBufferRTView, rgba_clear_colour);
 	// RENDER HERE
+	// Set Vertex Buffer // 03-01
+	UINT stride = sizeof(POS_COL_VERTEX);
+	UINT offset = 0;
+
+	g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
+
+	// Select which primitive type to use //03-01
+	g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// Draw the Vertex Buffer to the back buffer //03-01
+	g_pImmediateContext->Draw(3, 0);
+
+
 	// Display what has just been rendered
 	g_pSwapChain->Present(0, 0);
-}
+}
